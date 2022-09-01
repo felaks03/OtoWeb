dd156ec66491216c7bfa93569ef9bbd47b53e0fa
felaks03 <felixruizdelacuesta1@gmail.com>
2022-09-01 19:44:46 +0200

    scroll

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -7,9 +7,43 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor() { 
+    this.marginTimer
+  }
 
   ngOnInit(): void {
   }
 
+  margin: number = 0
+  setMargin(): any{
+    if(this.margin > -60){
+      if(this.margin == 0){
+        document.getElementById("radio-2")!.style.backgroundColor = "rgb(67, 180, 255)"
+        document.getElementById("radio-1")!.style.backgroundColor = "inherit"
+        this.margin += -20
+        document.getElementById("first")!.style.marginLeft = this.margin + "%"
+      } else if(this.margin == -20){
+        document.getElementById("radio-3")!.style.backgroundColor = "rgb(67, 180, 255)"
+        document.getElementById("radio-2")!.style.backgroundColor = "inherit"
+        this.margin += -20
+        document.getElementById("first")!.style.marginLeft = this.margin + "%"
+      } else if(this.margin == -40){
+        document.getElementById("radio-4")!.style.backgroundColor = "rgb(67, 180, 255)"
+        document.getElementById("radio-3")!.style.backgroundColor = "inherit"
+        this.margin += -20
+        document.getElementById("first")!.style.marginLeft = this.margin + "%"
+      }
+    } else {
+      document.getElementById("radio-1")!.style.backgroundColor = "rgb(67, 180, 255)"
+      document.getElementById("radio-4")!.style.backgroundColor = "inherit"
+      this.margin = 0
+      document.getElementById("first")!.style.marginLeft = this.margin + "%"
+    }
+  }
+  marginTimer = setInterval(this.setMargin, 10000);
+
+  clickRad($event: any){
+    this.margin = $event.target.value
+    document.getElementById("first")!.style.marginLeft = this.margin + "%"
+  }
 }
