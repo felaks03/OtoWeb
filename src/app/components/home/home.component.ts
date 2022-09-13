a41d289f3cee25cef64ec988dc162cf9cf39c20b
felaks03 <felixruizdelacuesta1@gmail.com>
2022-09-13 13:50:12 +0200

    home mejorado

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -11,11 +11,13 @@ export class HomeComponent implements OnInit {
   constructor() { 
     
   }
+  margin : number = 0
+  isActive: boolean = false
 
   ngOnInit(): void {
   }
 
-  margin : number = 0
+  
   clickRadio($event: any){
     document.getElementById("radio-1")!.style.backgroundColor = "inherit"
     document.getElementById("radio-2")!.style.backgroundColor = "inherit"
@@ -23,8 +25,65 @@ export class HomeComponent implements OnInit {
     document.getElementById("radio-4")!.style.backgroundColor = "inherit"
     document.getElementById($event.target.id)!.style.backgroundColor = "rgb(67, 180, 255)"
     this.margin = $event.target.value
-    console.log(this.margin)
     document.getElementById("first")!.style.marginLeft = this.margin + "%"
-    
+    document.getElementById("first")!.style.transition = "margin-left 0.8s ease-out"
+    if (this.margin == 0){
+      document.getElementById("text2")!.style.transform = "translateY(50px)"
+      document.getElementById("text2")!.style.color = "rgba(255, 255, 255, 0)"
+
+      document.getElementById("text3")!.style.transform = "translateY(50px)"
+      document.getElementById("text3")!.style.color = "rgba(255, 255, 255, 0)"
+
+      document.getElementById("text4")!.style.transform = "translateY(50px)"
+      document.getElementById("text4")!.style.color = "rgba(255, 255, 255, 0)"
+
+      document.getElementById("text1")!.style.transform = "translateY(-50px)"
+      document.getElementById("text1")!.style.color = "white"
+
+      document.getElementById("text1")!.style.transition = "all 1.5s ease-out"
+    } if (this.margin == -20){
+      document.getElementById("text1")!.style.transform = "translateY(50px)"
+      document.getElementById("text1")!.style.color = "rgba(255, 255, 255, 0)"
+
+      document.getElementById("text3")!.style.transform = "translateY(50px)"
+      document.getElementById("text3")!.style.color = "rgba(255, 255, 255, 0)"
+
+      document.getElementById("text4")!.style.transform = "translateY(50px)"
+      document.getElementById("text4")!.style.color = "rgba(255, 255, 255, 0)"
+
+      document.getElementById("text2")!.style.transform = "translateY(-50px)"
+      document.getElementById("text2")!.style.color = "white"
+
+      document.getElementById("text2")!.style.transition = "all 1.5s ease-out"
+    } if (this.margin == -40){
+      document.getElementById("text1")!.style.transform = "translateY(50px)"
+      document.getElementById("text1")!.style.color = "rgba(255, 255, 255, 0)"
+
+      document.getElementById("text2")!.style.transform = "translateY(50px)"
+      document.getElementById("text2")!.style.color = "rgba(255, 255, 255, 0)"
+
+      document.getElementById("text4")!.style.transform = "translateY(50px)"
+      document.getElementById("text4")!.style.color = "rgba(255, 255, 255, 0)"
+
+      document.getElementById("text3")!.style.transform = "translateY(-50px)"
+      document.getElementById("text3")!.style.color = "white"
+
+      document.getElementById("text3")!.style.transition = "all 1.5s ease-out"
+    } if (this.margin == -60){
+      document.getElementById("text1")!.style.transform = "translateY(50px)"
+      document.getElementById("text1")!.style.color = "rgba(255, 255, 255, 0)"
+
+      document.getElementById("text2")!.style.transform = "translateY(50px)"
+      document.getElementById("text2")!.style.color = "rgba(255, 255, 255, 0)"
+
+      document.getElementById("text3")!.style.transform = "translateY(50px)"
+      document.getElementById("text3")!.style.color = "rgba(255, 255, 255, 0)"
+
+      document.getElementById("text4")!.style.transform = "translateY(-50px)"
+      document.getElementById("text4")!.style.color = "white"
+
+      document.getElementById("text4")!.style.transition = "all 1.5s ease-out"
+    } 
   }
+  
 }
